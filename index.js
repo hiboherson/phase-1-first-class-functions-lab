fa6a85a476b6560dc8fa68830a4b6c9f424125f8
hiboherson <hiboherson@gmail.com>
2024-05-23 19:52:50 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+module.exports = fareDoubler;
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+module.exports = fareTripler;
+
+function selectDifferentDrivers(arrayOfDrivers, selectionFunction) {
+    return selectionFunction(arrayOfDrivers);
+}
+
+module.exports = selectDifferentDrivers;
+
+
